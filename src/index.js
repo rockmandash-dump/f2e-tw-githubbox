64b1d7b5736f9302e869c64fc081af765f616f80
Joseph Wang <jamesxxxxxxx@gmail.com>
2020-04-23 01:52:47 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+VANTA.NET({
+  el: "#vanta-net",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x6bd96b,
+  backgroundColor: 0xebebeb,
+  points: 7.0,
+  maxDistance: 24.0,
+  spacing: 17.0,
+});
